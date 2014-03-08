8f5a7af0f6c68fb980400db990a287fdca0457c6
Jon Schlinkert <dev@sellside.com>
2014-03-07 22:14:27 -0500

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+/*
+ * assemble-pattern-lab <https://github.com/assemble/assemble-pattern-lab>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+
+module.exports = function(grunt) {
+
+  'use strict';
+
+	// Project configuration.
+	grunt.initConfig({
+
+    // Project metadata
+    pkg: grunt.file.readJSON('package.json'),
+		site: grunt.file.readYAML('.assemble.yml'),
+
+    assemble: {
+      options: {
+        flatten: true,
+        assets: '<%= site.assets %>',
+
+        // Metadata
+        site: '<%= site %>',
+        data: ['<%= site.data %>/**/*.json'],
+        helpers: ['<%= site.helpers %>/*.js'],
+        plugins: '<%= site.plugins %>',
+
+        // Templates
+        layouts: '<%= site.layouts %>',
+        layout: '<%= site.layout %>',
+
+        // Pattern Lab templates
+        patterns: {
+          atoms: ['<%= site.atoms %>/**/*.hbs'],
+          molecules: ['<%= site.molecules %>/**/*.hbs'],
+          organisms: ['<%= site.organisms %>/**/*.hbs'],
+        }
+      },
+
+      site: {
+        src: ['<%= site.pages %>/*.hbs'],
+        dest: '<%= site.dest %>/'
+      },
+
+      patterns: {
+        options: {
+          permalinks: {
+            structure: ':00-:basename.:ext'
+          }
+        },
+        src: ['patterns/**/*.hbs'],
+        dest: '<%= site.dest %>/patterns/'
+      }
+    },
+
+    clean: {
+      examples: ['<%= assemble.examples.dest %>/**']
+    }
+	});
+
+	// Load Assemble
+	grunt.task.loadNpmTasks('assemble');
+
+  // The default task to run with the `grunt` command
+	grunt.registerTask('default', ['assemble']);
+};
---
+++ patterns/_helpers/patterns.js
@@ -0,0 +1,35 @@
+/**
+ * Handlebars Helpers for Pattern Lab
+ * Copyright (c) 2014 Jon Schlinkert
+ * Licensed under the MIT License (MIT).
+ */
+'use strict';
+
+var path = require('path');
+var file = require('fs-utils');
+var _ = require('lodash');
+
+
+module.exports.register = function (Handlebars, options, params) {
+  options = options || {};
+  var config = _.extend(options, options.data || {});
+
+  var patterns = ['atom', 'molecule', 'organism'];
+
+  patterns.forEach(function(pattern) {
+    var inflection = pattern + 's';
+
+    file.expand(options.patterns[inflection]).map(function(filepath) {
+      var name = pattern + '-' + file.base(filepath);
+      var template = file.readFileSync(filepath);
+      Handlebars.registerPartial(name, template);
+    });
+
+    Handlebars.registerHelper(pattern, function(name, context) {
+      context = _.extend(config, context || {});
+      var template = Handlebars.partials[pattern + '-' + name];
+      var fn = Handlebars.compile(template);
+      return new Handlebars.SafeString(fn(context));
+    });
+  });
+};
